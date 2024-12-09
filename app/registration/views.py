30cd3deec04c0d0ff24b9fda023d04358c576000
makmodol1173 <makmodol1173@gmail.com>
2024-12-10 03:05:24 +0600

    validation added

--- app/registration/views.py
+++ app/registration/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib import messages
 
 def registration(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        
+        if not name or not email or not password:
+            messages.error(request, "All fields are required")
+            return render(request, 'registration.html')
+        
+
+    
     return render(request, 'registration.html')
