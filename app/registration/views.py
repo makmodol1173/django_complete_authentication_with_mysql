81d4e3b1777b0c3ea5b1f4d43c0ac2012d230d03
makmodol1173 <makmodol1173@gmail.com>
2024-12-10 04:03:07 +0600

    user created and cookie added

--- app/registration/views.py
+++ app/registration/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.db import connection
+from decouple import config
+import bcrypt
 
 def registration(request):
+    auth_token = request.COOKIES.get(config('COOKIE_KEY'))
+    if auth_token:
+        return redirect('/profile') 
+
     if request.method == 'POST':
         name = request.POST.get("name")
         email = request.POST.get("email")
@@ -16,7 +22,20 @@ def registration(request):
             query = "SELECT * FROM users WHERE email = %s"
             cursor.execute(query,(email,))
             data = cursor.fetchone()
-            print(data)
+            
+            if data:
+                messages.error(request, "Email already exists")
+                return render(request, 'registration.html')
+            
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt()).decode('utf-8')
+            
+            insert_query = "INSERT INTO users (name, email, password, role) VALUES (%s, %s, %s, %s)"
+            cursor.execute(insert_query, (name, email, hashed_password, "user"))
+            connection.commit() 
+            
+            response = redirect("/profile")
+            response.set_cookie(config('COOKIE_KEY'), email, max_age=3600, httponly=True, secure=True)
+            return response
         
         print(name, email, password)
 
