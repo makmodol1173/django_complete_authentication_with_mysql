a94f5ddd6835604244702761f18c8b7b698f8fd9
makmodol1173 <makmodol1173@gmail.com>
2024-12-10 18:53:29 +0600

    profile added

--- app/profile/views.py
+++ app/profile/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.db import connection
+from decouple import config
 
 def profile(request):
-    return render(request, 'profile.html')
+    
+    auth_token = request.COOKIES.get(config('COOKIE_KEY'))
+    if not auth_token:
+        return redirect('/login')
+        
+    with connection.cursor() as cursor:
+        query = "SELECT * FROM users WHERE email = %s"
+        cursor.execute(query,(auth_token,))
+        data = cursor.fetchone()
+        user = {'name':data[1], 'email': data[2]}
+    return render(request, 'profile.html',user)
